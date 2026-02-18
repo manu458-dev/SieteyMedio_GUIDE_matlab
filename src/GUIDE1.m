d4f9c83ce96ed00f85365df19b769cc702a52480
manu458-dev <s.jmjm33.com@gmail.com>
2026-02-18 14:04:56 -0600

    se ha creado la interfaz base, con los componentes elementales.

---
+++ src/GUIDE1.m
@@ -0,0 +1,196 @@
+function varargout = GUIDE1(varargin)
+% GUIDE1 MATLAB code for GUIDE1.fig
+%      GUIDE1, by itself, creates a new GUIDE1 or raises the existing
+%      singleton*.
+%
+%      H = GUIDE1 returns the handle to a new GUIDE1 or the handle to
+%      the existing singleton*.
+%
+%      GUIDE1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUIDE1.M with the given input arguments.
+%
+%      GUIDE1('Property','Value',...) creates a new GUIDE1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before GUIDE1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to GUIDE1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help GUIDE1
+
+% Last Modified by GUIDE v2.5 18-Feb-2026 13:58:16
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @GUIDE1_OpeningFcn, ...
+                   'gui_OutputFcn',  @GUIDE1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before GUIDE1 is made visible.
+function GUIDE1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to GUIDE1 (see VARARGIN)
+
+% Choose default command line output for GUIDE1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes GUIDE1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+handles.axes3.Toolbar.Visible = 'off';
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = GUIDE1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
+
+% CONFIGURACIÓN DE FONDO DE PANTALLA
+%   setBackgroundWindow(hObject);   -
+
+%%%% SUPPORT FUNCTIONS %%%%
+handles.axes1.Toolbar.Visible = 'off';
+
+
+%% CONFIGURACION DE FONDO DE PANTALLA
+% Recibe como parametro el objeto componente sobre el que se quieref
+% configurar
+function setBackgroundWindow(hObject)
+ % 1) Que el axes cubra toda la figura
+    set(hObject, 'Units','normalized', ...
+                 'Position',[0 0 1 1]);
+
+    % 2) Cargar imagen
+    basePath = fileparts(mfilename('fullpath'));
+    imgPath  = fullfile(basePath, './green-background1.jpg'); % cambia por tu archivo
+    I = imread(imgPath);
+
+    % 3) Dibujar imagen estirada
+    image('Parent', hObject, 'CData', I);
+
+    % Forzar que ocupe TODO el axes
+    set(hObject, 'XLim', [1 size(I,2)], ...
+                 'YLim', [1 size(I,1)], ...
+                 'YDir', 'reverse');   % importante para que no se invierta
+
+    % Ajuste para que se estire
+    set(hObject, 'Position',[0 0 1 1]);
+    axis(hObject, 'off');
+    set(hObject, 'Units','normalized');
+
+    % 4) Que no bloquee botones
+    set(hObject, 'HitTest','off');
+    set(findobj(hObject,'Type','image'),'HitTest','off');
+
+    % 5) Mandar al fondo
+    uistack(hObject,'bottom');
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf("Has presionado el botón de 'JUEVO NUEVO'\n");
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf("Has presionado el botón de 'SIMULACIÓN'\n");
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf("Has presionado el botón de 'SALIR'\n");
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf("Has presionado el botón de 'PEDIR MÁS CARTAS'\n");
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf("Has presionado el botón de 'PLANTARSE'\n");
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function axes3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes3
+handles.axes3.Toolbar.Visible = 'off';
